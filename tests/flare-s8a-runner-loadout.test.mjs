86c18d9a6cc467c9a2d94835a2456c1890713ec3
coachtomlim <120995598+coachtomlim@users.noreply.github.com>
2026-09-13 10:45:51 +0800

    test: update runner loadout for shield and armor pieces

--- tests/flare-s8a-runner-loadout.test.mjs
+++ tests/flare-s8a-runner-loadout.test.mjs
@@ -2,14 +2,14 @@ import test from 'node:test';
 import assert from 'node:assert/strict';
 import {validateEquip,loadoutView} from '../public/flare-s8a/runner-loadout.mjs';
 
-test('player can equip owned weapon or armor only into governed slots',()=>{
-  const assets=[{playerId:'p1',assetKey:'blade-1'},{playerId:'p1',assetKey:'mail-1'}];
-  assert.deepEqual(validateEquip({playerId:'p1',runnerId:'r1',slot:'weapon',assetId:'blade-1',offer:{slot:'weapon'},assets}),{playerId:'p1',runnerId:'r1',slot:'weapon',assetId:'blade-1'});
-  assert.deepEqual(loadoutView({weapon:'blade-1',armor:'mail-1'}),{weapon:'blade-1',armor:'mail-1'});
+test('loadout exposes weapon shield and individual armor slots',()=>{
+  const assets=[{playerId:'p1',assetKey:'club-1'},{playerId:'p1',assetKey:'shield-1'},{playerId:'p1',assetKey:'boots-1'}];
+  assert.deepEqual(validateEquip({playerId:'p1',runnerId:'r1',slot:'shield',assetId:'shield-1',offer:{slot:'shield'},assets}),{playerId:'p1',runnerId:'r1',slot:'shield',assetId:'shield-1'});
+  assert.deepEqual(loadoutView({weapon:'club-1',shield:'shield-1',feet:'boots-1'}),{weapon:'club-1',shield:'shield-1',head:null,chest:null,hands:null,legs:null,feet:'boots-1'});
 });
 
 test('cross-player or wrong-slot equip fails closed',()=>{
-  const assets=[{playerId:'p2',assetKey:'blade-1'}];
-  assert.throws(()=>validateEquip({playerId:'p1',runnerId:'r1',slot:'weapon',assetId:'blade-1',offer:{slot:'weapon'},assets}));
-  assert.throws(()=>validateEquip({playerId:'p2',runnerId:'r1',slot:'armor',assetId:'blade-1',offer:{slot:'weapon'},assets}));
+  const assets=[{playerId:'p2',assetKey:'shield-1'}];
+  assert.throws(()=>validateEquip({playerId:'p1',runnerId:'r1',slot:'shield',assetId:'shield-1',offer:{slot:'shield'},assets}));
+  assert.throws(()=>validateEquip({playerId:'p2',runnerId:'r1',slot:'head',assetId:'shield-1',offer:{slot:'shield'},assets}));
 });
